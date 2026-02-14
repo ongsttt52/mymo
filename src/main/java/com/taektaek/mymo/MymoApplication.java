4d591c588eded851665e976bfa042a74f5c617ff
ongsttt52 <ongsttt52@gmail.com>
2026-02-14 16:09:08 +0900

    chore: spring boot initialize setting

---
+++ src/main/java/com/taektaek/mymo/MymoApplication.java
@@ -0,0 +1,13 @@
+package com.taektaek.mymo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MymoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MymoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/taektaek/mymo/MymoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taektaek.mymo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MymoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
